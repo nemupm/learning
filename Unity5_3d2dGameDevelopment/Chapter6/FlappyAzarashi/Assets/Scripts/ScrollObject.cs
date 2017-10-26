fc6c055776122f6869557dc5a739c8c0793a0fc2
nemupm <nemupm@gmail.com>
2017-10-27 00:11:42 +0900

    [flappy-azarashi] added scroll

---
+++ Unity5_3d2dGameDevelopment/Chapter6/FlappyAzarashi/Assets/Scripts/ScrollObject.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollObject : MonoBehaviour
+{
+	public float speed = 1.0f;
+	public float startPosition;
+	public float endPosition;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate (-1 * speed * Time.deltaTime, 0, 0);
+
+		if (transform.position.x <= endPosition) ScrollEnd();
+	}
+
+	void ScrollEnd()
+	{
+		transform.Translate(-1 * (endPosition - startPosition), 0, 0);
+
+		SendMessage("OnScrollEnd", SendMessageOptions.DontRequireReceiver);
+		
+		
+	}
+}
